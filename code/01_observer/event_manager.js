403fdb9e4d6a6ff1cc23fe9ddd1aadcfdf22c86b
JayChenFE <chenjiehwy@163.com>
2019-05-04 13:33:33 +0800

    3

--- code/01_observer/event_manager.js
+++ code/01_observer/event_manager.js
@@ -1,79 +1,79 @@
 //ES5
-var EventManager = (function() {
-	var events = {};
+var EventManager = (function () {
+    var events = {};
 
-	function on(evt, handler) {
+    function on(evt, handler) {
 
-		if (typeof handler !== 'function') {
-			throw new TypeError('handler is not a function!');
-			return;
-		}
+        if (typeof handler !== 'function') {
+            throw new TypeError('handler is not a function!');
+            return;
+        }
 
-		events[evt] = events[evt] || [];
-		events[evt].push(handler);
-	}
+        events[evt] = events[evt] || [];
+        events[evt].push(handler);
+    }
 
-	function trigger(evt, args) {
-		if (!events[evt]) {
-			return;
-		}
+    function trigger(evt, args) {
+        if (!events[evt]) {
+            return;
+        }
 
-		events[evt].forEach(function(handler) {
-			handler(args);
-		})
-	}
+        events[evt].forEach(function (handler) {
+            handler(args);
+        })
+    }
 
-	function off(evt) {
-		delete events[evt];
-	}
+    function off(evt) {
+        delete events[evt];
+    }
 
-	return {
-		on: on,
-		trigger: trigger,
-		off: off
-	};
+    return {
+        on: on,
+        trigger: trigger,
+        off: off
+    };
 })();
 
 
 //ES6
 const EventManager = ((() => {
-	const events = {};
+    const events = {};
 
-	function on(evt, handler) {
+    function on(evt, handler) {
 
-		if (typeof handler !== 'function') {
-			throw new TypeError('handler is not a function!');
-			return;
-		}
+        if (typeof handler !== 'function') {
+            throw new TypeError('handler is not a function!');
+            return;
+        }
 
-		events[evt] = events[evt] || [];
-		events[evt].push(handler);
-	}
+        events[evt] = events[evt] || [];
+        events[evt].push(handler);
+    }
 
-	function trigger(evt, args) {
-		if (!events[evt]) {
-			return;
-		}
+    function trigger(evt, args) {
+        if (!events[evt]) {
+            return;
+        }
 
-		events[evt].forEach(handler => {
-			handler(args);
-		})
-	}
+        events[evt].forEach(handler => {
+            handler(args);
+        })
+    }
 
-	function off(evt) {
-		delete events[evt];
-	}
+    function off(evt) {
+        delete events[evt];
+    }
 
-	return {
-		on,
-		trigger,
-		off
-	};
+    return {
+        on,
+        trigger,
+        off
+    };
 }))();
 
 //调用
-EventManager.on('text:change', function(val) {
-	console.log('text:change...  now val is ' + val);
+EventManager.on('text:change', function (val) {
+    console.log('text:change...  now val is ' + val);
 });
 EventManager.trigger('text:change', '饥人谷');
 EventManager.off('text:change');
--- code/01_observer/logging_observer.js
+++ code/01_observer/logging_observer.js
@@ -1,31 +1,31 @@
-$.ajax('http://xxx.com?login', function(data) {
-  login.trigger('loginSucc', data);
+$.ajax('http://xxx.com?login', function (data) {
+    login.trigger('loginSucc', data);
 });
 
-var header = (function() { // header模块
-  login.listen('loginSucc', function() {
-    header.setAvatar(data.avatar);
-  })
-  return setAvatar: function(avatar) {
-    console.log('设置header模块的头像')
-  }
+var header = (function () { // header模块
+    login.listen('loginSucc', function () {
+        header.setAvatar(data.avatar);
+    })
+    return setAvatar: function(avatar) {
+        console.log('设置header模块的头像')
+    }
 })();
 
 
-var nav = (function() { // nav模块
-  login.listen('loginSucc', function() {
-    nav.setAvatar(data.avatar);
-  })
-  return setAvatar: function(avatar) {
-    console.log('设置nav模块的头像')
-  }
+var nav = (function () { // nav模块
+    login.listen('loginSucc', function () {
+        nav.setAvatar(data.avatar);
+    })
+    return setAvatar: function(avatar) {
+        console.log('设置nav模块的头像')
+    }
 })();
 
-var address = (function() { //address模块
-  login.listen('loginSucc', function() {
-    address.refresh();
-  })
-  return refresh: function() {
-    console.log('刷新收货地址列表')
-  }
+var address = (function () { //address模块
+    login.listen('loginSucc', function () {
+        address.refresh();
+    })
+    return refresh: function() {
+        console.log('刷新收货地址列表')
+    }
 })();
